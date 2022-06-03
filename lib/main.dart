7b4e1b351abf287b459e40cd67b40fa00fc233b2
FlutLab.io <>
2022-06-03 02:22:41 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:karanganbunga/screen/splash_screen.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+void main() => runApp(MyApp());
 
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'UTS Dafid Ahmad Fauzi',
+      // Application name
+      title: 'Flutter Stateful Clicker Counter',
       theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
+        // Application theme data, you can set the colors for the application as
+        // you want
+        primarySwatch: Colors.blue,
       ),
-      home: const SplashScreen(),
+      home: SplashScreen(),
     );
   }
 }
