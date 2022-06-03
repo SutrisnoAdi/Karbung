e4745cf7cb822c2e1a6c01e24cc818da2e8d73f0
FlutLab.io <>
2022-06-03 02:16:05 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
         // you want
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Clicker Coun'),
+      home: SplashScreen(),
     );
   }
 }
